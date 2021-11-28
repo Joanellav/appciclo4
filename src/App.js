f253910520b60087be0076440f5035d5a06e2ef5
Joanellav <90978702+Joanellav@users.noreply.github.com>
2021-11-27 19:21:32 -0500

    Aproximacion Componentes Plantilla 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 
+function Despachos(){
+  return(
+    <div id="despachos">Banner</div> 
+  )  
+}
+
 function App() {
   return (
-    <div className="App">Hello World
+    <div>This is our AppCiclo4: <Despachos/>
     </div>
   );
 }
