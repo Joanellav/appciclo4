49e9360d71719b308bf7eb0b550009c67905d07a
Joanellav <90978702+Joanellav@users.noreply.github.com>
2021-11-28 13:10:11 -0500

    Pruebas 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes} from 'react-router-dom';
 import './App.css';
+
+//Components
+//import xxxx from '-/component/xxxx';
 
 function Despachos(){
   return(
@@ -6,11 +11,20 @@ function Despachos(){
   )  
 }
 
-function App() {
-  return (
-    <div>This is our AppCiclo4: <Despachos/>
+class App extends Component{
+  render(){
+    return <div>
+      <div>This is our AppCiclo4: <Despachos/>
+      </div>
+    <Router>
+      <Routes exact path="/"  render ={()=>{
+        return
+        //Componentes          
+        }}>
+      </Routes>
+    </Router>
     </div>
-  );
+  }
 }
 
 export default App;
